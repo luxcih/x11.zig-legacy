3d39b2fbd63a912c422d582a67cd884eb0fa4cfd
Luxcih <luxcih.me@gmail.com>
2026-08-30 21:48:52 +0800

    Add focused events example

---
+++ examples/events.zig
@@ -0,0 +1,23 @@
+const std = @import("std");
+const x11 = @import("x11");
+
+pub fn main() !void {
+    const event = try x11.Event.parse(&.{
+        2, 38, 0, 0,
+        0, 0, 0, 0,
+        0, 0, 0, 0,
+        0, 0, 0, 0,
+        0, 0, 0, 0,
+        10, 0, 20, 0,
+        30, 0, 40, 0,
+        0, 0, 1, 0,
+    }, .little);
+
+    switch (event) {
+        .key_press => |key| std.debug.print(
+            "Parsed KeyPress: keycode {} at ({}, {})\n",
+            .{ key.detail, key.event_x, key.event_y },
+        ),
+        else => unreachable,
+    }
+}
