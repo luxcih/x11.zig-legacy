09f1cde7a726aa3c7fb5846e69fb0eeff6c9f190
Luxcih <luxcih.me@gmail.com>
2026-08-30 23:57:24 +0800

    Use Connection response header reader in examples

--- examples/query_tree.zig
+++ examples/query_tree.zig
@@ -12,8 +12,7 @@ pub fn main(init: std.process.Init) !void {
 
     var read_buffer: [4096]u8 = undefined;
     var reader = session.connection.reader(init.io, &read_buffer);
-    var header_bytes: [x11.Window.QueryTree.reply_header_size]u8 = undefined;
-    try reader.interface.readSliceAll(&header_bytes);
+    const header_bytes = try session.connection.readResponseHeader(reader);
 
     const header = try x11.Window.QueryTree.ReplyHeader.parse(&header_bytes, session.byte_order);
     const allocator = std.heap.page_allocator;
