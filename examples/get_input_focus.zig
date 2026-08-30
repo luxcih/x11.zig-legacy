57c9bfb163950472149ea85c282f8233ef21afab
Luxcih <luxcih.me@gmail.com>
2026-08-31 00:06:03 +0800

    Pass response readers by reference

--- examples/get_input_focus.zig
+++ examples/get_input_focus.zig
@@ -10,8 +10,8 @@ pub fn main(init: std.process.Init) !void {
     try session.connection.writeAll(init.io, try x11.Input.GetInputFocus.encode(&request_buffer, session.byte_order));
 
     var read_buffer: [64]u8 = undefined;
-    const reader = session.connection.reader(init.io, &read_buffer);
-    const reply_bytes = try session.connection.readResponseHeader(reader);
+    var reader = session.connection.reader(init.io, &read_buffer);
+    const reply_bytes = try session.connection.readResponseHeader(&reader);
 
     const reply = try x11.Input.GetInputFocus.Reply.parse(&reply_bytes, session.byte_order);
     std.debug.print("Input focus: window=0x{x}, revert_to={s}\n", .{ reply.focus, @tagName(reply.revert_to) });
