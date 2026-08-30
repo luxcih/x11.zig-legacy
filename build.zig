2ae5b3b9fedcf01d6403c263497254d47febdb26
Luxcih <luxcih.me@gmail.com>
2026-08-30 22:01:16 +0800

    Refactor example build registration

--- build.zig
+++ build.zig
@@ -4,15 +4,18 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const module = b.addModule("x11", .{
+    const x11 = b.addModule("x11", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const tests = b.addTest(.{ .root_module = module });
+    const tests = b.addTest(.{
+        .root_module = x11,
+    });
     const run_tests = b.addRunArtifact(tests);
-    const test_step = b.step("test", "Run tests");
+
+    const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&run_tests.step);
 
     const example_names = [_][]const u8{
@@ -26,39 +29,56 @@ pub fn build(b: *std.Build) void {
         "unix_socket",
     };
 
-    const examples_step = b.step("examples", "Build all examples");
-    const check_examples_step = b.step("check-examples", "Compile all examples");
+    const check_examples = b.step(
+        "check-examples",
+        "Compile all examples",
+    );
 
-    inline for (example_names) |name| {
-        const source = b.fmt("examples/{s}.zig", .{name});
-        const executable_name = b.fmt("example-{s}", .{name});
-
-        const example_module = b.createModule(.{
-            .root_source_file = b.path(source),
-            .target = target,
-            .optimize = optimize,
-        });
-        example_module.addImport("x11", module);
-
-        const example = b.addExecutable(.{
-            .name = executable_name,
-            .root_module = example_module,
-        });
-
-        const run_example = b.addRunArtifact(example);
-
-        const run_step_name = b.fmt("example-{s}", .{name});
-        const run_step_description = b.fmt(
-            "Run the {s} example",
-            .{name},
+    for (example_names) |name| {
+        addExample(
+            b,
+            x11,
+            target,
+            optimize,
+            name,
+            check_examples,
         );
-        const example_step = b.step(
-            run_step_name,
-            run_step_description,
-        );
-        example_step.dependOn(&run_example.step);
-
-        examples_step.dependOn(&example.step);
-        check_examples_step.dependOn(&example.step);
     }
+
+    const examples = b.step("examples", "Build all examples");
+    examples.dependOn(&check_examples.step);
+}
+
+fn addExample(
+    b: *std.Build,
+    x11: *std.Build.Module,
+    target: std.Build.ResolvedTarget,
+    optimize: std.builtin.OptimizeMode,
+    name: []const u8,
+    check_examples: *std.Build.Step,
+) void {
+    const source = b.fmt("examples/{s}.zig", .{name});
+    const executable_name = b.fmt("example-{s}", .{name});
+
+    const example_module = b.createModule(.{
+        .root_source_file = b.path(source),
+        .target = target,
+        .optimize = optimize,
+    });
+    example_module.addImport("x11", x11);
+
+    const example = b.addExecutable(.{
+        .name = executable_name,
+        .root_module = example_module,
+    });
+
+    const run_example = b.addRunArtifact(example);
+
+    const run_step = b.step(
+        b.fmt("example-{s}", .{name}),
+        b.fmt("Run the {s} example", .{name}),
+    );
+    run_step.dependOn(&run_example.step);
+
+    check_examples.dependOn(&example.step);
 }
