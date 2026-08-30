df420562f765fa3b94889428ce08e922b305b0ae
Luxcih <luxcih.me@gmail.com>
2026-08-30 16:02:59 +0800

    Use Connection in Unix socket example

--- examples/unix_socket.zig
+++ examples/unix_socket.zig
@@ -2,27 +2,24 @@ const std = @import("std");
 const x11 = @import("x11");
 
 pub fn main(init: std.process.Init) !void {
-    const io = init.io;
-
     const display = try x11.Display.parse(":0");
 
     const tmpdir = init.environ_map.get("TMPDIR") orelse
         return error.MissingTmpDir;
 
-    var path_buffer: [std.fs.max_path_bytes]u8 = undefined;
-    const path = try std.fmt.bufPrint(
-        &path_buffer,
-        "{s}/.X11-unix/X{}",
-        .{ tmpdir, display.number },
+    var socket_dir_buffer: [std.fs.max_path_bytes]u8 = undefined;
+    const socket_dir = try std.fmt.bufPrint(
+        &socket_dir_buffer,
+        "{s}/.X11-unix",
+        .{tmpdir},
     );
 
-    const address = try std.Io.net.UnixAddress.init(path);
+    var connection = try x11.Connection.connectLocal(
+        init.io,
+        display,
+        socket_dir,
+    );
+    defer connection.close(init.io);
 
-    var stream = try address.connect(io);
-    defer stream.socket.close(io);
-
-    std.debug.print("Connected to display {} at {s}.\n", .{
-        display.number,
-        path,
-    });
+    std.debug.print("Connected to display {}.\n", .{display.number});
 }
