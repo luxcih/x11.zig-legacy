8e1c50bb829fea4480ce4c79d03ad528cae7f278
Luxcih <luxcih.me@gmail.com>
2026-08-30 15:37:13 +0800

    Add Unix socket connection experiment

---
+++ examples/unix_socket.zig
@@ -0,0 +1,15 @@
+const std = @import("std");
+
+pub fn main() !void {
+    var threaded = std.Io.Threaded.init(std.heap.page_allocator);
+    defer threaded.deinit();
+
+    const io = threaded.io();
+
+    const address = try std.Io.net.UnixAddress.init("/tmp/.X11-unix/X0");
+
+    var stream = try address.connect(io);
+    defer stream.socket.close(io);
+
+    std.debug.print("Connected to the X server socket.\n", .{});
+}
