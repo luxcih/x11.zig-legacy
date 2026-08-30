af4212db6a138fc7d4486021fdefde1560db4c41
Luxcih <luxcih.me@gmail.com>
2026-08-31 01:24:57 +0800

    fix: compile property examples

--- examples/list_properties.zig
+++ examples/list_properties.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const x11 = @import("x11");
+const Wire = @import("../src/Wire.zig");
 const common = @import("common.zig");
 
 pub fn main(init: std.process.Init) !void {
@@ -20,12 +21,12 @@ pub fn main(init: std.process.Init) !void {
     var reader = session.connection.reader(init.io, &read_buffer);
 
     const header = try session.connection.readResponseHeader(&reader);
-    const count = x11.Wire.readU16(header[8..10], session.byte_order);
+    const count = Wire.readU16(header[8..10], session.byte_order);
 
-    var atoms_bytes = try init.arena.allocator().alloc(u8, @as(usize, count) * 4);
+    const atoms_bytes = try init.arena.allocator().alloc(u8, @as(usize, count) * 4);
     try reader.interface.readSliceAll(atoms_bytes);
 
-    var reply = try x11.Window.ListProperties.Reply.parse(
+    const reply = try x11.Window.ListProperties.Reply.parse(
         init.arena.allocator(),
         &header,
         atoms_bytes,
