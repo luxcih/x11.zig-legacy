ca1045fa081afb0b644d6235d88e506931ecd205
Luxcih <luxcih.me@gmail.com>
2026-08-31 01:28:47 +0800

    fix: keep Wire internal in property examples

--- examples/list_properties.zig
+++ examples/list_properties.zig
@@ -1,6 +1,5 @@
 const std = @import("std");
 const x11 = @import("x11");
-const Wire = @import("../src/Wire.zig");
 const common = @import("common.zig");
 
 pub fn main(init: std.process.Init) !void {
@@ -21,7 +20,7 @@ pub fn main(init: std.process.Init) !void {
     var reader = session.connection.reader(init.io, &read_buffer);
 
     const header = try session.connection.readResponseHeader(&reader);
-    const count = Wire.readU16(header[8..10], session.byte_order);
+    const count = try x11.Window.ListProperties.Reply.atomCount(&header, session.byte_order);
 
     const atoms_bytes = try init.arena.allocator().alloc(u8, @as(usize, count) * 4);
     try reader.interface.readSliceAll(atoms_bytes);
