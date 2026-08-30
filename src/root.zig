78bc1ee8fdb51b47411d48bce175368adf6e7bb6
Luxcih <luxcih.me@gmail.com>
2026-08-30 16:20:02 +0800

    Export PixmapFormat type

--- src/root.zig
+++ src/root.zig
@@ -5,3 +5,4 @@ pub const Connection = @import("Connection.zig").Connection;
 pub const Setup = @import("Setup.zig").Setup;
 pub const SetupResponse = @import("SetupResponse.zig").SetupResponse;
 pub const SetupSuccess = @import("SetupSuccess.zig").SetupSuccess;
+pub const PixmapFormat = @import("PixmapFormat.zig").PixmapFormat;
