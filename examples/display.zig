38bef6f9376efcb2fbce6369c2af9beae29ccf01
Luxcih <luxcih.me@gmail.com>
2026-08-30 15:26:03 +0800

    Add display parsing example

---
+++ examples/display.zig
@@ -0,0 +1,21 @@
+const std = @import("std");
+const x11 = @import("x11");
+
+pub fn main() !void {
+    const display = try x11.Display.parse("tcp/example.org:0.1");
+
+    std.debug.print(
+        \\protocol: {s}
+        \\host: {s}
+        \\separator: {s}
+        \\number: {}
+        \\screen: {}
+        \\
+    , .{
+        display.protocol orelse "(none)",
+        display.host,
+        @tagName(display.separator),
+        display.number,
+        display.screen,
+    });
+}
