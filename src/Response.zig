7ec10905ceb8a589d35cb6c1bb8b0add470f22e7
Luxcih <luxcih.me@gmail.com>
2026-08-30 23:52:32 +0800

    Fix reserved keyword in response type enum

--- src/Response.zig
+++ src/Response.zig
@@ -2,13 +2,13 @@ const std = @import("std");
 
 /// The category of a 32-byte message received from an X11 server.
 pub const Type = enum {
-    error,
+    protocol_error,
     reply,
     event,
 
     pub fn classify(response_type: u8) Type {
         return switch (response_type & 0x7f) {
-            0 => .error,
+            0 => .protocol_error,
             1 => .reply,
             else => .event,
         };
@@ -16,7 +16,7 @@ pub const Type = enum {
 };
 
 test "classify X11 response types" {
-    try std.testing.expectEqual(Type.error, Type.classify(0));
+    try std.testing.expectEqual(Type.protocol_error, Type.classify(0));
     try std.testing.expectEqual(Type.reply, Type.classify(1));
     try std.testing.expectEqual(Type.event, Type.classify(2));
     try std.testing.expectEqual(Type.event, Type.classify(127));
