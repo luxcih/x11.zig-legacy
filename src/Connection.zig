85530fc1930fa87982609ab13931ce831c665694
Luxcih <luxcih.me@gmail.com>
2026-08-30 16:02:53 +0800

    Add local connection support

--- src/Connection.zig
+++ src/Connection.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const Display = @import("Display.zig").Display;
 
 pub const Connection = struct {
     stream: std.Io.net.Stream,
@@ -7,6 +8,24 @@ pub const Connection = struct {
         return .{
             .stream = stream,
         };
+    }
+
+    pub fn connectLocal(
+        io: std.Io,
+        display: Display,
+        socket_dir: []const u8,
+    ) !Connection {
+        var path_buffer: [std.fs.max_path_bytes]u8 = undefined;
+        const path = try std.fmt.bufPrint(
+            &path_buffer,
+            "{s}/X{}",
+            .{ socket_dir, display.number },
+        );
+
+        const address = try std.Io.net.UnixAddress.init(path);
+        const stream = try address.connect(io);
+
+        return init(stream);
     }
 
     pub fn close(self: *Connection, io: std.Io) void {
