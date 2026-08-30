29aab51d8f93820570b5f94609dea7a7cb757e57
Luxcih <luxcih.me@gmail.com>
2026-08-31 01:13:51 +0800

    feat: add Atom.GetName

--- src/Atom.zig
+++ src/Atom.zig
@@ -67,6 +67,47 @@ pub const Intern = struct {
     };
 };
 
+pub const GetName = struct {
+    pub const EncodeError = error{BufferTooSmall};
+    pub const ParseError = error{InvalidLength, InvalidResponse};
+
+    pub const opcode = 17;
+    pub const request_size = 8;
+    pub const reply_size = 32;
+
+    atom: u32,
+
+    pub fn encode(
+        self: GetName,
+        buffer: []u8,
+        byte_order: ByteOrder,
+    ) EncodeError![]const u8 {
+        if (buffer.len < request_size)
+            return error.BufferTooSmall;
+
+        Wire.writeU16(buffer[0..2], opcode, byte_order);
+        Wire.writeU16(buffer[2..4], 2, byte_order);
+        Wire.writeU32(buffer[4..8], self.atom, byte_order);
+
+        return buffer[0..request_size];
+    }
+
+    pub const Reply = struct {
+        name_length: u16,
+
+        pub fn parsePrefix(bytes: []const u8, byte_order: ByteOrder) ParseError!Reply {
+            if (bytes.len != reply_size)
+                return error.InvalidLength;
+            if (bytes[0] != 1)
+                return error.InvalidResponse;
+
+            return .{
+                .name_length = Wire.readU16(bytes[8..10], byte_order),
+            };
+        }
+    };
+};
+
 test "encode little-endian InternAtom" {
     const request = Atom.Intern{
         .name = "WM_PROTOCOLS",
@@ -111,4 +152,27 @@ test "parse little-endian InternAtom reply" {
 
     const reply = try Atom.Intern.Reply.parse(&bytes, .little);
     try std.testing.expectEqual(@as(u32, 0x01020304), reply.atom);
+}
+
+
+test "encode little-endian GetAtomName" {
+    const request = Atom.GetName{ .atom = 0x01020304 };
+
+    var buffer: [Atom.GetName.request_size]u8 = undefined;
+    const encoded = try request.encode(&buffer, .little);
+
+    try std.testing.expectEqualSlices(u8, &.{
+        17, 0,
+        2, 0,
+        4, 3, 2, 1,
+    }, encoded);
+}
+
+test "parse GetAtomName reply prefix" {
+    var bytes: [Atom.GetName.reply_size]u8 = [_]u8{0} ** Atom.GetName.reply_size;
+    bytes[0] = 1;
+    Wire.writeU16(bytes[8..10], 12, .little);
+
+    const reply = try Atom.GetName.Reply.parsePrefix(&bytes, .little);
+    try std.testing.expectEqual(@as(u16, 12), reply.name_length);
 }
