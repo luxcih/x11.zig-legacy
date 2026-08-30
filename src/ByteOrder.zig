0d6221cf677e0c1864f72d20cf0e8be884341c97
Luxcih <luxcih.me@gmail.com>
2026-08-31 07:45:34 +0800

    refactor: remove redundant ByteOrder type

--- src/ByteOrder.zig
+++
@@ -1,8 +0,0 @@
-//! Endianness used by the X11 wire protocol.
-//!
-//! X11 supports the same little- and big-endian byte orders represented by
-//! Zig's builtin Endian type.
-
-const std = @import("std");
-
-pub const ByteOrder = std.builtin.Endian;
