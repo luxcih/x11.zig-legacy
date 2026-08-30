34f1eb82108d1da9d56ec50b218b320dcc8acf34
Luxcih <luxcih.me@gmail.com>
2026-08-30 16:15:53 +0800

    Export SetupResponse type

--- src/root.zig
+++ src/root.zig
@@ -3,3 +3,4 @@
 pub const Display = @import("Display.zig").Display;
 pub const Connection = @import("Connection.zig").Connection;
 pub const Setup = @import("Setup.zig").Setup;
+pub const SetupResponse = @import("SetupResponse.zig").SetupResponse;
