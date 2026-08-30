2cd21961b82e4101e0178507a6978ba77b126612
Luxcih <luxcih.me@gmail.com>
2026-08-30 16:00:38 +0800

    Use Display in Unix socket example

--- examples/unix_socket.zig
+++ examples/unix_socket.zig
@@ -1,7 +1,10 @@
 const std = @import("std");
+const x11 = @import("x11");
 
 pub fn main(init: std.process.Init) !void {
     const io = init.io;
+
+    const display = try x11.Display.parse(":0");
 
     const tmpdir = init.environ_map.get("TMPDIR") orelse
         return error.MissingTmpDir;
@@ -9,8 +12,8 @@ pub fn main(init: std.process.Init) !void {
     var path_buffer: [std.fs.max_path_bytes]u8 = undefined;
     const path = try std.fmt.bufPrint(
         &path_buffer,
-        "{s}/.X11-unix/X1",
-        .{tmpdir},
+        "{s}/.X11-unix/X{}",
+        .{ tmpdir, display.number },
     );
 
     const address = try std.Io.net.UnixAddress.init(path);
@@ -18,5 +21,8 @@ pub fn main(init: std.process.Init) !void {
     var stream = try address.connect(io);
     defer stream.socket.close(io);
 
-    std.debug.print("Connected to the X server socket at {s}.\n", .{path});
+    std.debug.print("Connected to display {} at {s}.\n", .{
+        display.number,
+        path,
+    });
 }
