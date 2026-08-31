c404f41f10f0231ba0d744801da27966a68100e9
Luxcih <luxcih.me@gmail.com>
2026-09-01 01:02:19 +0800

    refactor: orchestrate handshake through client I/O

--- src/Handshake.zig
+++ src/Handshake.zig
@@ -1,6 +1,7 @@
 //! Performs the one-time X11 connection handshake.
 
 const std = @import("std");
+const Client = @import("Client.zig");
 const Endian = std.builtin.Endian;
 const Wire = @import("Wire.zig");
 const Server = @import("Server.zig");
@@ -13,54 +14,51 @@ pub const Result = struct {
     resource_id_mask: u32,
 };
 
-pub fn perform(
-    allocator: std.mem.Allocator,
-    reader: anytype,
-    writer: anytype,
-    endian: Endian,
-) !Result {
-    try writeRequest(writer, endian);
+pub fn perform(client: *Client) !Result {
+    try client.send(Request{});
 
     var prefix: [8]u8 = undefined;
-    try reader.interface.readSliceAll(&prefix);
+    try client.reader.interface.readSliceAll(&prefix);
 
-    const additional_length = Wire.readU16(prefix[6..8], endian);
+    const additional_length = Wire.readU16(prefix[6..8], client.endian);
     const additional_bytes = @as(usize, additional_length) * 4;
 
-    const body = try allocator.alloc(u8, additional_bytes);
-    defer allocator.free(body);
-    try reader.interface.readSliceAll(body);
+    const body = try client.allocator.alloc(u8, additional_bytes);
+    defer client.allocator.free(body);
+    try client.reader.interface.readSliceAll(body);
 
     return switch (prefix[0]) {
         0 => error.SetupFailed,
         1 => .{
-            .server = try Server.parse(allocator, body, endian),
-            .resource_id_base = Wire.readU32(body[4..8], endian),
-            .resource_id_mask = Wire.readU32(body[8..12], endian),
+            .server = try Server.parse(client.allocator, body, client.endian),
+            .resource_id_base = Wire.readU32(body[4..8], client.endian),
+            .resource_id_mask = Wire.readU32(body[8..12], client.endian),
         },
         2 => error.AuthenticationRequired,
         else => error.InvalidSetupResponse,
     };
 }
 
-fn writeRequest(
-    writer: anytype,
-    endian: Endian,
-) !void {
-    var request: [12]u8 = undefined;
-    request[0] = switch (endian) {
-        .little => 'l',
-        .big => 'B',
-    };
-    request[1] = 0;
-    Wire.writeU16(request[2..4], 11, endian);
-    Wire.writeU16(request[4..6], 0, endian);
-    Wire.writeU16(request[6..8], 0, endian);
-    Wire.writeU16(request[8..10], 0, endian);
-    request[10] = 0;
-    request[11] = 0;
+const Request = struct {
+    pub fn encode(
+        _: Request,
+        writer: *std.Io.Writer,
+        endian: Endian,
+    ) !void {
+        var request: [12]u8 = undefined;
+        request[0] = switch (endian) {
+            .little => 'l',
+            .big => 'B',
+        };
+        request[1] = 0;
+        Wire.writeU16(request[2..4], 11, endian);
+        Wire.writeU16(request[4..6], 0, endian);
+        Wire.writeU16(request[6..8], 0, endian);
+        Wire.writeU16(request[8..10], 0, endian);
+        request[10] = 0;
+        request[11] = 0;
 
-    try writer.interface.writeAll(&request);
-    try writer.interface.flush();
-}
+        try writer.writeAll(&request);
+    }
+};
 
