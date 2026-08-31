4f19c0f42ca3f826fdde408753425606d37e2cff
Luxcih <luxcih.me@gmail.com>
2026-08-31 15:55:32 +0800

    fix: update display example terminology

--- examples/display.zig
+++ examples/display.zig
@@ -15,7 +15,7 @@ pub fn main() !void {
         display.protocol orelse "(none)",
         display.host,
         @tagName(display.separator),
-        display.number,
+        display.display_number,
         display.screen,
     });
 }
