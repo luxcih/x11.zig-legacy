c18442468922d240e845b3c81d7da856800ac55d
Luxcih <luxcih.me@gmail.com>
2026-08-31 23:48:26 +0800

    refactor: persist client reader and writer

--- src/Client.zig
+++ src/Client.zig
@@ -9,9 +9,31 @@ const XidAllocator = @import("XidAllocator.zig");
 
 const Client = @This();
 
+const IoState = struct {
+    connection: Connection,
+    read_buffer: [4096]u8 = undefined,
+    write_buffer: [1024]u8 = undefined,
+    reader: std.Io.net.Stream.Reader = undefined,
+    writer: std.Io.net.Stream.Writer = undefined,
+
+    fn init(
+        self: *IoState,
+        io: std.Io,
+        connection: Connection,
+    ) void {
+        self.connection = connection;
+        self.reader = self.connection.reader(io, &self.read_buffer);
+        self.writer = self.connection.writer(io, &self.write_buffer);
+    }
+
+    fn deinit(self: *IoState, io: std.Io) void {
+        self.connection.close(io);
+    }
+};
+
 io: std.Io,
 allocator: std.mem.Allocator,
-connection: Connection,
+io_state: *IoState,
 endian: std.builtin.Endian,
 setup: SetupInfo,
 xids: XidAllocator,
@@ -22,27 +44,27 @@ pub fn connect(
     allocator: std.mem.Allocator,
     display_name: []const u8,
 ) !Client {
-    var connection = try Connection.connect(io, display_name);
+    const io_state = try allocator.create(IoState);
+    errdefer allocator.destroy(io_state);
+
+    const connection = try Connection.connect(io, display_name);
     errdefer connection.close(io);
+
+    io_state.init(io, connection);
 
     const setup_request = Setup{};
 
-    var write_buffer: [1024]u8 = undefined;
-    var writer = connection.writer(io, &write_buffer);
-    try setup_request.encode(&writer.interface);
-    try writer.interface.flush();
-
-    var read_buffer: [4096]u8 = undefined;
-    var reader = connection.reader(io, &read_buffer);
+    try setup_request.encode(&io_state.writer.interface);
+    try io_state.writer.interface.flush();
 
     var prefix: [8]u8 = undefined;
-    try reader.interface.readSliceAll(&prefix);
+    try io_state.reader.interface.readSliceAll(&prefix);
 
     const response = try SetupResponse.parsePrefix(prefix, setup_request.endian);
 
     const additional = try allocator.alloc(u8, response.additionalBytes());
     defer allocator.free(additional);
-    try reader.interface.readSliceAll(additional);
+    try io_state.reader.interface.readSliceAll(additional);
 
     var setup_info = switch (response) {
         .success => try SetupInfo.parse(allocator, additional, setup_request.endian),
@@ -54,28 +76,25 @@ pub fn connect(
     return .{
         .io = io,
         .allocator = allocator,
-        .connection = connection,
+        .io_state = io_state,
         .endian = setup_request.endian,
         .setup = setup_info,
-        .xids = XidAllocator.init(setup_info.success.resource_id_base, setup_info.success.resource_id_mask),
+        .xids = XidAllocator.init(
+            setup_info.success.resource_id_base,
+            setup_info.success.resource_id_mask,
+        ),
     };
 }
 
 /// Encodes and sends an X11 request.
 pub fn send(self: *Client, request: anytype) !void {
-    var buffer: [1024]u8 = undefined;
-    var writer = self.connection.writer(self.io, &buffer);
-
-    try request.encode(&writer.interface, self.endian);
-    try writer.interface.flush();
+    try request.encode(&self.io_state.writer.interface, self.endian);
+    try self.io_state.writer.interface.flush();
 }
 
 /// Receives and decodes an X11 message.
 pub fn recv(self: *Client, comptime T: type) !T {
-    var buffer: [4096]u8 = undefined;
-    var reader = self.connection.reader(self.io, &buffer);
-
-    return T.decode(&reader.interface, self.endian);
+    return T.decode(&self.io_state.reader.interface, self.endian);
 }
 
 /// Returns the next X resource identifier available to this client.
@@ -86,5 +105,6 @@ pub fn nextXid(self: *Client) XidAllocator.Error!u32 {
 /// Releases resources owned by the client and closes its connection.
 pub fn deinit(self: *Client) void {
     self.setup.deinit(self.allocator);
-    self.connection.close(self.io);
+    self.io_state.deinit(self.io);
+    self.allocator.destroy(self.io_state);
 }
